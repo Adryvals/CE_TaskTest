dac0a5d0a3f1b383651a2d223facc64b6e04931e
Adrian Valdes <adrvs94@gmail.com>
2025-10-07 11:33:02 -0400

    feat(app) agregado clase de servicio para registrar las inyecciones de dependencias necesarias.

---
+++ CE_TaskTest.BackEnd/Services/Extensions/Services.cs
@@ -0,0 +1,27 @@
+﻿using CE_TaskTest.BackEnd.Services.Dtos.Request;
+using CE_TaskTest.BackEnd.Services.Validations;
+using FluentValidation;
+using System.Reflection;
+
+namespace CE_TaskTest.BackEnd.Services.Extensions
+{
+    public static class Services
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton(configuration);
+
+            // Mapper
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // Validations
+            services.AddScoped<IValidator<TareaRequestDto>, TareaValidation>();
+            services.AddScoped<IValidator<EstimacionRequestDto>, EstimacionValidation>();
+
+            // Services
+            services.AddScoped<TareaService>();
+
+            return services;
+        }
+    }
+}
